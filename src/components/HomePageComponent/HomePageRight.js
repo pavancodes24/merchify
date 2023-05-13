3d640a2d28b793333e4faa2e0dd471bad205955f
pavancodes24 <pavanchandupatla24@gmail.com>
2023-05-14 04:30:32 +0530

    size changes3

--- src/components/HomePageComponent/HomePageRight.js
+++ src/components/HomePageComponent/HomePageRight.js
@@ -10,7 +10,7 @@ const HomePageRight = () => {
         direction={"column"}
         alignItems={"center"}
         justifyContent={"space-around"}
-        // style={{ height: "13vh" }}
+        style={{ height: "13vh" }}
       >
         <Grid item>
           <Typography variant="caption">
