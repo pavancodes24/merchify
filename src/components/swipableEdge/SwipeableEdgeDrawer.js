689372c134cd087b1453c9e6eb779afcc24fada2
pavancodes24 <pavanchandupatla24@gmail.com>
2023-05-14 07:19:22 +0530

    added content in swipable drawer

--- src/components/swipableEdge/SwipeableEdgeDrawer.js
+++ src/components/swipableEdge/SwipeableEdgeDrawer.js
@@ -9,6 +9,7 @@ import Box from "@mui/material/Box";
 import Skeleton from "@mui/material/Skeleton";
 import Typography from "@mui/material/Typography";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import { Grid } from "@mui/material";
 
 const drawerBleeding = 56;
 
@@ -78,9 +79,7 @@ function SwipeableEdgeDrawer(props) {
           }}
         >
           <Puller />
-          <Typography sx={{ p: 2, color: "text.secondary" }}>
-            51 results
-          </Typography>
+          <Typography sx={{ p: 2 }}>Quiz Rules</Typography>
         </StyledBox>
         <StyledBox
           sx={{
@@ -90,13 +89,141 @@ function SwipeableEdgeDrawer(props) {
             overflow: "auto",
           }}
         >
-          <Skeleton variant="rectangular" height="100%" />
+          <Grid container direction="column" spacing={3}>
+            <Grid item>
+              <Grid
+                container
+                direction={"row"}
+                gap="10px"
+                alignItems={"center"}
+              >
+                <Grid item>
+                  <img src="/timeicon.png" alt="" width={30} height={30} />
+                </Grid>
+                <Grid item>
+                  <Grid container direction={"column"}>
+                    <Grid item>
+                      {" "}
+                      <Typography style={{ fontWeight: 600 }}>
+                        10 Mins
+                      </Typography>
+                    </Grid>
+                    <Grid item>
+                      <Typography style={{ width: "300px" }}>
+                        Keep in mind that it is a time-bound quiz.
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Grid
+                container
+                direction={"row"}
+                gap="10px"
+                alignItems={"center"}
+              >
+                <Grid item>
+                  <img src="/questionicon.png" alt="" width={30} height={30} />
+                </Grid>
+                <Grid item>
+                  <Grid container direction={"column"}>
+                    <Grid item>
+                      {" "}
+                      <Typography style={{ fontWeight: 600 }}>
+                        5 Questions
+                      </Typography>
+                    </Grid>
+                    <Grid item style={{ width: "300px" }}>
+                      <Typography>We believe that you will ace it!.</Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Grid
+                container
+                direction={"row"}
+                gap="10px"
+                alignItems={"center"}
+              >
+                <Grid item>
+                  {" "}
+                  <img src="/winsave.png" alt="" width={30} height={30} />
+                </Grid>
+                <Grid item>
+                  <Grid container direction={"column"}>
+                    <Grid item>
+                      <Typography style={{ fontWeight: 600 }}>
+                        50 % Passing Criteria.
+                      </Typography>
+                    </Grid>
+                    <Grid item>
+                      <Typography style={{ width: "300px" }}>
+                        All the best! See you on the other side.
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid item>
+              <Grid
+                container
+                direction="row"
+                gap="10px"
+                style={{ background: "green", padding: "10px" }}
+                justifyContent={"center"}
+                alignItems={"center"}
+              >
+                <Grid item>
+                  <div
+                    style={{
+                      width: "20px",
+                      height: "20px",
+                      borderRadius: "10px",
+                      background: "red",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    i
+                  </div>
+                </Grid>
+                <Grid item>
+                  <Typography style={{ width: "300px", fontSize: "13px" }}>
+                    This quiz can only be attempted once daily.
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid item>
+              <Grid
+                container
+                justifyContent={"center"}
+                alignItems={"center"}
+                style={{ paddingBottom: "30px" }}
+              >
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    sx={{ borderRadius: "20px", width: "120px" }}
+                  >
+                    Start
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
         </StyledBox>
       </SwipeableDrawer>
     </Root>
   );
 }
-
-
 
 export default SwipeableEdgeDrawer;
